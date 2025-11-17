f119d234587ab5f26cdad27e4efb03f8f36f1575
Meriem ELK <m.elkouarir@gmail.com>
2025-11-17 12:05:56 -0500

    Mise à jour des variables CSS et du design Tailwind dans la vue principale - Accueil

--- frontend/src/pages/Bienvenue.jsx
+++ frontend/src/pages/Bienvenue.jsx
@@ -3,56 +3,93 @@ import { Link } from "react-router-dom";
 function Bienvenue() {
   return (
     <main className="
-        flex items-end justify-center
-        py-10 px-5
-        bg-[linear-gradient(0deg,rgba(0,0,0,1)25%,rgba(0,0,0,0)),url('../assets/images/heroBienvenue.webp')] bg-cover bg-center min-h-screen">
+      // Disposition + Espacement
+      flex items-end justify-center min-h-screen py-10 px-5
+      // Typographie
+      font-[family-name:var(--font-body)]
+      // Visual
+      bg-[linear-gradient(0deg,rgba(0,0,0,0.7)25%,rgba(0,0,0,0)),url('../assets/images/heroBienvenue.webp')] bg-cover bg-center
+      ">
+      
       <section className="
+        // Disposition + Espacement
         w-full max-w-md">
+
         {/* Texte de bienvenue */}
         <header className="
-          text-center
-          mb-8
-          text-primary-foreground">
+          // Disposition + Espacement
+          text-center mb-8
+          // Visual
+          text-[color:var(--color-principal-premier-plan)]
+          // Interactif
+          ">
+          
           <p className="
-            text-sm font-normal
+            // Disposition + Espacement
             mb-1
-            opacity-95">
+            // Typographie
+            text-[length:var(--taille-petit)] font-normal
+            ">
             Votre cave personnelle, organisée
           </p>
+          
           <p className="
-            text-sm font-normal
+            // Disposition + Espacement
             mb-8
-            opacity-95">
+            // Typographie
+            text-[length:var(--taille-petit)] font-normal
+            ">
             comme vous le souhaitez
           </p>
 
           <h1 className="
-            text-5xl font-bold
-            mb-12">
+            // Disposition + Espacement
+            mb-12
+            // Typographie
+            text-[length:var(--taille-tres-gros)] font-[family-name:var(--font-display)] font-bold
+            ">
             Bienvenue à Vino
           </h1>
         </header>
 
         {/* Boutons d'action */}
         <div className="
-          flex flex-col
-          gap-4 ">
+          // Disposition + Espacement
+          flex flex-col gap-4
+          ">
 
-          {/* Bouton Secondaire */}
+          {/* Bouton Secondaire - Se connecter */}
           <Link to="/connexion" className="
-              text-center block
-              px-8 py-3
-              font-semibold
-              bg-[#821250] text-white rounded-lg shadow-md
-              hover:bg-[#A8427D] hover:shadow-lg hover:-translate-y-0.5 transition-all duration-300 cursor-pointer">
+            // Disposition + Espacement
+            block text-center px-8 py-3
+            // Typographie
+            text-[length:var(--taille-normal)] font-semibold
+            // Visual
+            bg-[color:var(--color-principal-200)] text-[color:var(--color-fond)] border-1 border-[color:var(--color-principal-200)]  rounded-[length:var(--arrondi-grand)] shadow-md
+            // Interactif
+            hover:bg-[color:var(--color-principal-premier-plan)] hover:text-[color:var(--color-principal-300)] hover:shadow-lg hover:-translate-y-0.5 transition-all duration-300 cursor-pointer">
             Se connecter
           </Link>
 
           {/* Lien "Pas encore de compte?" */}
-          <div className="text-center mt-4">
-            <p className="text-sm text-gray-300">
+          <div className="
+            // Disposition + Espacement
+            text-center mt-4
+            ">
+            <p className="
+              // Typographie
+              text-[length:var(--taille-petit)]
+              // Visual
+              text-[color:var(--color-principal-premier-plan)]
+              ">
               Pas encore de compte?{' '}
-              <Link to="/inscription" className="text-[#A8427D] font-semibold hover:text-[#F5DFE5]">
+              <Link to="/inscription" className="
+                // Typographie
+                font-semibold
+                // Visual
+                text-[color:var(--color-principal-200)]
+                // Interactif
+                hover:text-[color:var(--color-principal-100)] transition-colors">
                 Je m'inscris
               </Link>
             </p>
