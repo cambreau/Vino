be5c7cf9ab6f9a316244fa4c20c96f7576407304
Meriem ELK <m.elkouarir@gmail.com>
2025-11-17 14:32:36 -0500

    Création des composants bouton et retourBouton, et application du bouton sur la page Bienvenue

--- frontend/src/pages/Bienvenue.jsx
+++ frontend/src/pages/Bienvenue.jsx
@@ -1,11 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Bouton from "../components/components-partages/Formulaire/FormulaireBouton/Bouton";
 
 function Bienvenue() {
+  const navigate = useNavigate();
+
   return (
     <main className="
       flex items-end justify-center min-h-screen py-10 px-5
       font-[family-name:var(--font-body)]
-      bg-[linear-gradient(0deg,rgba(0,0,0,0.7)25%,rgba(0,0,0,0)),url('../assets/images/heroBienvenue.webp')] bg-cover bg-center
+       bg-[linear-gradient(0deg,rgba(0,0,0,0.7)25%,rgba(0,0,0,0)),url('../assets/images/heroBienvenue.webp')] bg-cover bg-center
       ">
       
       <section className="w-full max-w-md">
@@ -41,14 +44,13 @@ function Bienvenue() {
         {/* Boutons d'action */}
         <div className="flex flex-col gap-4">
 
-          {/* Bouton Secondaire - Se connecter */}
-          <Link to="/connexion" className="
-            block text-center px-8 py-3
-            text-[length:var(--taille-normal)] font-semibold
-            bg-[color:var(--color-principal-200)] text-[color:var(--color-fond)] border-1 border-[color:var(--color-principal-200)]  rounded-[length:var(--arrondi-grand)] shadow-md
-            hover:bg-[color:var(--color-principal-premier-plan)] hover:text-[color:var(--color-principal-300)] hover:shadow-lg hover:-translate-y-0.5 transition-all duration-300 cursor-pointer">
-            Se connecter
-          </Link>
+         
+          {/* Bouton Primaire - Se connecter */}
+          <Bouton 
+            texte="Se connecter"
+            type="primaire"
+            action={() => navigate('/connexion')}
+          />
 
           {/* Lien "Pas encore de compte?" */}
           <div className="text-center mt-4">
