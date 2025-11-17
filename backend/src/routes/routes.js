d0261695a2cdf7716495e45c2a1a974cc8ab2677
cambreau <breau.camille76@gmail.com>
2025-11-17 11:45:21 -0500

    Mise a jour de la structure des routes

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,7 +1,10 @@
-import express from 'express';
+import express from "express";
+
+import utilisateurRoutes from "./utilisateur.routes.js";
 
 const router = express.Router();
 
 // Routes backend de VINO
+router.use("/utilisateurs", utilisateurRoutes);
 
 export default router;
