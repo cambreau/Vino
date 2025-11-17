e9263558b456c6aaa05f7faab6cdfbee3af4b236
Danilo Costa <dancc86@gmail.com>
2025-11-17 18:17:12 +0000

    Importation du composant Icon et mise à jour du code pour l’appeler dynamiquement avec le nom de l’icône souhaitée.

--- frontend/src/components/components-partages/MenuEnBas/MenuEnBas.jsx
+++ frontend/src/components/components-partages/MenuEnBas/MenuEnBas.jsx
@@ -1,7 +1,15 @@
+import Icon from "../Icon/Icon";
+
 function MenuEnBas({}) {
-    return <footer>
-      
-  </footer>;
+  return (
+    <footer>
+      <Icon name="accueil" />
+      <Icon name="cellier" />
+      <Icon name="liste" />
+      <Icon name="recherche" />
+      <Icon name="utilisateur" />
+    </footer>
+  );
 }
 
 export default MenuEnBas;
