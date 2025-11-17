4b1d16cffafc689467edb5f9f968cdfca7e74cfb
cambreau <breau.camille76@gmail.com>
2025-11-17 11:23:32 -0500

    Creation des routes utilisateurs

---
+++ backend/src/routes/utilisateur.routes.js
@@ -0,0 +1,24 @@
+import {
+  creerUtilisateur,
+  recupererUtilisateur,
+  recupererUtilisateurParEmail,
+  modifierUtilisateur,
+  supprimerUtilisateur,
+} from "../controllers/controller.utilisateur.js";
+
+// POST / - Creer un utilisateur
+router.post("/", creerUtilisateur);
+
+// GET //:id - Recuperer un utilisateur
+router.get("/:id", recupererUtilisateur);
+
+// GET //email/:email - Trouver par email
+router.get("/email/:email", recupererUtilisateurParEmail);
+
+// PUT //:id - Mettre à jour un utilisateur
+router.put("/:id", modifierUtilisateur);
+
+// DELETE //:id - Supprimer un utilisateur
+router.delete("/:id", supprimerUtilisateur);
+
+export default router;
