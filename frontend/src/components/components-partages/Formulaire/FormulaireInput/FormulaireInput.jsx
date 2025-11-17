18a3c7cf71117b73ba45015dd5052c6fb54564b4
cambreau <breau.camille76@gmail.com>
2025-11-17 09:12:03 -0500

    Création FormulaireInput

---
+++ frontend/src/components/components-partages/Formulaire/FormulaireInput/FormulaireInput.jsx
@@ -0,0 +1,28 @@
+// Utils
+import { formatteStringSansAccentEtEspace } from "../../../../lib/utils";
+
+function FormulaireInput({
+  type,
+  nom,
+  genre,
+  estObligatoire,
+  onChange,
+  value = "",
+}) {
+  return (
+    <div>
+      <label htmlFor={formatteStringSansAccentEtEspace(nom)}>{nom} :</label>
+      <input
+        type={type}
+        name={formatteStringSansAccentEtEspace(nom)}
+        id={formatteStringSansAccentEtEspace(nom)}
+        {...(estObligatoire ? { required: true } : {})}
+        placeholder={`Entrez ${genre} ${nom.toLowerCase()}`}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+export default FormulaireInput;
