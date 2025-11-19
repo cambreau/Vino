08a83592cb791fe5d4fe2b7d4258a12a5fc89295
cambreau <breau.camille76@gmail.com>
2025-11-19 10:10:55 -0500

    Creation du formulaire modification - sur template formulaire inscription

--- frontend/src/pages/ModificationProfil.jsx
+++ frontend/src/pages/ModificationProfil.jsx
@@ -1,5 +1,212 @@
+import Formulaire from "../components/components-partages/Formulaire/Formulaire";
+import FormulaireInput from "../components/components-partages/Formulaire/FormulaireInput/FormulaireInput";
+import Bouton from "../components/components-partages/Boutons/Bouton";
+import Message from "../components/components-partages/Message/Message";
+import { regex, validationChamp } from "../lib/validationFormulaire.js";
+import { useState } from "react";
+import { useSearchParams, useParams, useNavigate } from "react-router-dom";
+
 function ModificationProfil() {
-  return <div>ModificationProfil</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const echec = searchParams.get("echec") === "true";
+
+  //Recuperer id dans l'url
+  const { id } = useParams();
+  // Les informations utilisateur
+  const [utilisateur, setUtilisateur] = useState({
+    id: id,
+    nom: "",
+    courriel: "",
+    mot_de_passe: "",
+  });
+  // Confirmation du mot de passe (non envoyée au backend)
+  const [confirmation, setConfirmation] = useState("");
+
+  /**
+   * Recuperer les informations de l'utilisateur cote backend.
+   */
+  useEffect(() => {
+    const chargerUtilisateur = async () => {
+      const utilisateur = await recupererUtilisateur(id); // Fonction a creer requetes.js
+      if (utilisateur) {
+        setUtilisateur({
+          id: utilisateur.id,
+          nom: utilisateur.nom || "",
+          courriel: utilisateur.courriel || "",
+          mot_de_passe: mot_de_passe || "",
+        });
+      }
+    };
+    chargerUtilisateur();
+  }, []);
+
+  // Regroupe les messages d'erreurs à afficher.
+  const [erreurs, setErreurs] = useState({
+    nom: "",
+    courriel: "",
+    confirmation: "",
+    motDePasse: "",
+  });
+
+  /**
+   * Fonction envoie la modification du profil de l'utilisateur
+   * @param {Event} e - L'événement de soumission du formulaire
+   */
+  const envoieModificationProfil = async (e) => {
+    e.preventDefault();
+
+    // Envoyer les données utilisateur
+    await modifierUtilisateur(utilisateur, navigate); // Creer fonction requetes.js
+  };
+
+  /**
+   * Ferme le message d'erreur en supprimant le paramètre "echec" de l'URL.
+   */
+  const fermerMessage = () => {
+    searchParams.delete("echec");
+    setSearchParams(searchParams);
+  };
+
+  return (
+    <section
+      className="
+        min-h-screen px-(--rythme-serre) pb-(--rythme-espace) grid grid-rows-[1fr_5fr] items-end
+        "
+    >
+      {echec && (
+        <Message
+          texte="Une erreur est survenue lors de la modification. Veuillez réessayer."
+          type="erreur"
+          onClose={fermerMessage}
+        />
+      )}
+      <Formulaire
+        titreFormulaire="Modification de votre profil"
+        method="POST"
+        action={envoieModificationProfil}
+        enfants={
+          <>
+            <FormulaireInput
+              type="text"
+              nom="nom"
+              genre="un"
+              estObligatoire={true}
+              value={utilisateur.nom}
+              onChange={(e) => {
+                setUtilisateur((prev) => ({ ...prev, nom: e.target.value }));
+              }}
+              onBlur={(e) => {
+                const valeur = e.target.value;
+                if (!validationChamp(regex.regNom, valeur)) {
+                  const erreur =
+                    "Le nom doit comporter entre 2 et 50 caractères, uniquement des lettres, des accents et le tiret (-).";
+                  setErreurs((prev) => ({ ...prev, nom: erreur }));
+                } else {
+                  setErreurs((prev) => ({ ...prev, nom: "" }));
+                }
+              }}
+            />
+            {erreurs.nom && <Message texte={erreurs.nom} type="erreur" />}
+
+            <FormulaireInput
+              type="email"
+              nom="courriel"
+              genre="un"
+              estObligatoire={true}
+              value={utilisateur.courriel}
+              onChange={(e) => {
+                const valeur = e.target.value;
+                setUtilisateur((prev) => ({ ...prev, courriel: valeur }));
+                if (!validationChamp(regex.regcourriel, valeur)) {
+                  const erreur = "Veuillez saisir un courriel valide.";
+                  setErreurs((prev) => ({ ...prev, courriel: erreur }));
+                } // Ne pas oublier de valider que l'adresse courriel est unique
+                else {
+                  setErreurs((prev) => ({ ...prev, courriel: "" }));
+                }
+              }}
+            />
+            {erreurs.courriel && (
+              <Message texte={erreurs.courriel} type="erreur" />
+            )}
+
+            <FormulaireInput
+              type="text"
+              nom="mot_de_passe"
+              genre="un"
+              estObligatoire={true}
+              value={utilisateur.mot_de_passe}
+              onChange={(e) => {
+                setUtilisateur((prev) => ({
+                  ...prev,
+                  mot_de_passe: e.target.value,
+                }));
+              }}
+              onBlur={(e) => {
+                const valeur = e.target.value;
+                if (!validationChamp(regex.regMotDePasse, valeur)) {
+                  const erreur =
+                    "Le mot de passe doit contenir au moins 8 caractères, dont une lettre majuscule, une lettre minuscule et un caractère spécial";
+                  setErreurs((prev) => ({ ...prev, motDePasse: erreur }));
+                } else {
+                  setErreurs((prev) => ({ ...prev, motDePasse: "" }));
+                }
+              }}
+            />
+            {erreurs.motDePasse && (
+              <Message texte={erreurs.motDePasse} type="erreur" />
+            )}
+
+            <FormulaireInput
+              type="text"
+              nom="confirmation"
+              genre="une"
+              estObligatoire={true}
+              value={confirmation}
+              onChange={(e) => {
+                setConfirmation(e.target.value);
+              }}
+              onBlur={(e) => {
+                const valeur = e.target.value;
+                if (utilisateur.mot_de_passe !== valeur) {
+                  const erreur =
+                    "La confirmation du mot de passe n'est pas valide";
+                  setErreurs((prev) => ({
+                    ...prev,
+                    confirmation: erreur,
+                  }));
+                } else {
+                  setErreurs((prev) => ({ ...prev, confirmation: "" }));
+                }
+              }}
+            />
+            {erreurs.confirmation && (
+              <Message texte={erreurs.confirmation} type="erreur" />
+            )}
+          </>
+        }
+        bouton={
+          <>
+            <Bouton
+              taille="moyen"
+              texte="Annuler"
+              type="secondaire"
+              typeHtml="button"
+              action={reinitialiserFormulaire}
+            />
+            <Bouton
+              taille="moyen"
+              texte="Enregistrer"
+              type="primaire"
+              typeHtml="button"
+              action={envoieModificationProfil}
+            />
+          </>
+        }
+      />
+    </section>
+  );
 }
 
 export default ModificationProfil;
