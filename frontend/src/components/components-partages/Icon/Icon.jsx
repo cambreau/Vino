ebdd356059e815cf31b97674a7df976271136ca5
Danilo Costa <dancc86@gmail.com>
2025-11-19 13:43:20 +0000

    Correction de l'utilisation des variables tailwind

--- frontend/src/components/components-partages/Icon/Icon.jsx
+++ frontend/src/components/components-partages/Icon/Icon.jsx
@@ -32,14 +32,14 @@ function Icon({ nom, size = 24, couleur, typeMenu }) {
     <div>
       {/* Verifier le si c'est le menu en haut ou en bas pour avoir la bonne affichage d'ensemble du icon et nom correspondent */}
       {typeMenu === "bas" ? (
-        <div className="flex flex-col items-center gap-[var(--rythme-tres-serre)] w-full text-[${couleur}]">
+        <div className="flex flex-col items-center gap-(--rythme-tres-serre) w-full text-[${couleur}]">
           <IconComponent size={size} color={couleur} />
           {typeMenu === "bas" ? (
             <p style={{ color: couleur }}>{IconNomMajuscule(nom)}</p>
           ) : null}
         </div>
       ) : (
-        <div className="flex items-center gap-[var(--rythme-base)] w-full text-[${couleur}]">
+        <div className="flex items-center gap-(--rythme-base) w-full text-[${couleur}]">
           <IconComponent size={size} color={couleur} />
           {/* Affiche le texte seulement si nom n’est PAS menuHamburger ou Fermer */}
           {nom !== "menuHamburger" &&
