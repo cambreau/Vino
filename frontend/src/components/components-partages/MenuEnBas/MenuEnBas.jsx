9ac27d04245eecae44d2203af25d455f770407fe
GabMaisonneuve <e2496359@cmaisonneuve.qc.ca>
2025-11-19 13:31:52 -0500

    interface profil, style ajouté, manque Icone et lien href pour compléter

--- frontend/src/components/components-partages/MenuEnBas/MenuEnBas.jsx
+++ frontend/src/components/components-partages/MenuEnBas/MenuEnBas.jsx
@@ -4,18 +4,10 @@ function MenuEnBas({}) {
   return (
     <footer className="flex justify-around fixed bottom-0 max-w-[500px] mx-auto inset-x-0 p-(--rythme-base) bg-(--color-principal-300)">
       <Link href="#">
-        <Icon
-          nom="accueil"
-          couleur="(--color-principal-100)"
-          typeMenu="bas"
-        />
+        <Icon nom="accueil" couleur="(--color-principal-100)" typeMenu="bas" />
       </Link>
       <Link href="#">
-        <Icon
-          nom="cellier"
-          couleur="(--color-principal-100)"
-          typeMenu="bas"
-        />
+        <Icon nom="cellier" couleur="(--color-principal-100)" typeMenu="bas" />
       </Link>
       <Link href="#">
         <Icon nom="liste" couleur="(--color-principal-100)" typeMenu="bas" />
@@ -28,11 +20,7 @@ function MenuEnBas({}) {
         />
       </Link>
       <Link href="#">
-        <Icon
-          nom="profil"
-          couleur="(--color-principal-100)"
-          typeMenu="bas"
-        />
+        <Icon nom="profil" couleur="(--color-principal-100)" typeMenu="bas" />
       </Link>
     </footer>
   );
