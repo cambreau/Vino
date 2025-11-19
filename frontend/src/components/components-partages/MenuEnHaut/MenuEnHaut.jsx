bec2334fe2294f153da504480799568507462996
Danilo Costa <dancc86@gmail.com>
2025-11-19 18:57:20 +0000

    Ajout de l’option de recherche dans le menu du haut avec un menu déroulant affichant la barre de recherche au clic sur l’icône de loupe.
    Ajout d’un fond gris derrière les menus hamburger et recherche pour améliorer la lisibilité et la hiérarchie visuelle.
    
    Suppression de la classe fixed top-0 de la navigation pour éviter les comportements indésirables.
    
    Déplacement de l’icône X afin qu’elle soit intégrée directement dans les menus déroulants pour une fermeture plus intuitive.

--- frontend/src/components/components-partages/MenuEnHaut/MenuEnHaut.jsx
+++ frontend/src/components/components-partages/MenuEnHaut/MenuEnHaut.jsx
@@ -1,13 +1,22 @@
 import { useState } from "react";
 import Icon from "../Icon/Icon";
 import RasinLogo from "../../../assets/images/grape_logo.svg";
+import FormulaireInput from "../Formulaire/FormulaireInput/FormulaireInput";
 
 function MenuEnHaut({}) {
   // Dans la propos envoyer les infos de la personne connecter pour les afficher au menu
   const [estMenuOuvert, setestMenuOuvert] = useState(false);
+  const [estRechercheOuvert, setestRechercheOuvert] = useState(false);
+
+  // VOIR COMMENT UTILISER LE FORMINPUT ---- POUR ENLEVER LE BUG
+  const handleChange = (e) => {
+    setRecherche(e.target.value);
+  };
+
+  const handleBlur = (e) => {};
 
   return (
-    <nav className="flex items-center justify-between fixed top-0 max-w-[500px] mx-auto inset-x-0 p-(--rythme-base) bg-(--color-fond-secondaire)">
+    <nav className="flex items-center justify-between max-w-[500px] mx-auto inset-x-0 p-(--rythme-base) bg-(--color-fond-secondaire)">
       <div className="relative">
         {/* Bouton hamburger */}
         <button
@@ -15,32 +24,46 @@ function MenuEnHaut({}) {
           aria-label={estMenuOuvert ? "Fermer le menu" : "Ouvrir le menu"}
           aria-expanded={estMenuOuvert}
         >
-          {!estMenuOuvert ? (
-            <Icon
-              nom="menuHamburger"
-              typeMenu="haut"
-              couleur="(--color-principal-300)"
-            />
-          ) : (
-            <Icon
-              nom="fermer"
-              typeMenu="haut"
-              couleur="(--color-principal-300)"
-            />
-          )}
+          <Icon
+            nom="menuHamburger"
+            typeMenu="haut"
+            couleur="(--color-principal-300)"
+          />
         </button>
+
+        {/* Overlay gris (voile) */}
+        {estMenuOuvert && (
+          <div
+            className="fixed inset-0 bg-black/50"
+            onClick={() => setestMenuOuvert(false)}
+          />
+        )}
 
         {/* Menu déroulant */}
         {estMenuOuvert && (
-          <div className="absolute mt-(--rythme-tres-serre) py-(--rythme-base) min-w-[300px] bg-(--color-fond-secondaire)">
-            <header className="mb-[var(--rythme-espace)]">
-              <h2 className="text-(--color-principal-300) text-(length:--taille-grand) font-display font-bold">
-                Utilisateur
-              </h2>
-              <small className="text-(--color-principal-300) text-(length:--taille-moyen) font-display">
-                Courriel
-              </small>
-            </header>
+          <div className="absolute -left-(--rythme-base) mt-(--rythme-tres-serre) p-(--rythme-base) min-w-[300px] bg-(--color-fond-secondaire)">
+            <div className="flex justify-between mb-(--rythme-espace)">
+              <header>
+                <h2 className="text-(--color-principal-300) text-(length:--taille-grand) font-display font-bold">
+                  Utilisateur
+                </h2>
+                <small className="text-(--color-principal-300) text-(length:--taille-moyen) font-display">
+                  Courriel
+                </small>
+              </header>
+              <button
+                className="absolute right-(--rythme-base) top-(--rythme-serre)"
+                onClick={() => setestMenuOuvert(!estMenuOuvert)}
+                aria-label={estMenuOuvert ? "Fermer le menu" : "Ouvrir le menu"}
+                aria-expanded={estMenuOuvert}
+              >
+                <Icon
+                  nom="fermer"
+                  typeMenu="haut"
+                  couleur="(--color-principal-300)"
+                />
+              </button>
+            </div>
             <div className="flex flex-col gap-(--rythme-base)">
               <a href="#">
                 <Icon
@@ -83,15 +106,72 @@ function MenuEnHaut({}) {
         <img src={RasinLogo} alt="Logo raisin" width="43" height="35" />
       </header>
 
-      {/* Déconnexion */}
+      {/* Bouton Recherche */}
+      <div className="flex items-center gap-(--rythme-base) mb-(--rythme-tres-serre) relative">
+        <button
+          onClick={() => setestRechercheOuvert(!estRechercheOuvert)}
+          aria-label={
+            estRechercheOuvert ? "Fermer la recherche" : "Ouvrir la recherche"
+          }
+        >
+          <Icon
+            nom="recherche"
+            couleur="(--color-principal-300)"
+            typeMenu="haut"
+          />
+        </button>
 
-      <button aria-label="Deconnection">
-        <Icon
-          nom="deconnection"
-          typeMenu="haut"
-          couleur="(--color-principal-300)"
-        />
-      </button>
+        {/* Overlay gris */}
+        {estRechercheOuvert && (
+          <div
+            className="fixed inset-0 bg-black/50"
+            onClick={() => setestRechercheOuvert(false)}
+          />
+        )}
+
+        {/* Menu de recherche déroulant */}
+        {estRechercheOuvert && (
+          <div className=" flex absolute -right-(--rythme-base) top-full  mt-(--rythme-tres-serre) p-(--rythme-espace) px-(--rythme-base) min-w-[300px] bg-(--color-fond-secondaire)">
+            {/* <input
+              type="search"
+              placeholder="Rechercher un vin..."
+              className="w-full p-[var(--rythme-serre)] border rounded-lg"
+              autoFocus
+            /> */}
+            <FormulaireInput
+              type="test"
+              nom=""
+              genre="un"
+              estObligatoire={true}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <button
+              className="absolute right-(--rythme-base) top-(--rythme-serre)"
+              onClick={() => setestRechercheOuvert(!estRechercheOuvert)}
+              aria-label={
+                estRechercheOuvert ? "Fermer recherche" : "Ouvrir recherche"
+              }
+              aria-expanded={estRechercheOuvert}
+            >
+              <Icon
+                nom="fermer"
+                typeMenu="haut"
+                couleur="(--color-principal-300)"
+              />
+            </button>
+          </div>
+        )}
+
+        {/* Déconnexion */}
+        <button aria-label="Déconnexion">
+          <Icon
+            nom="deconnection"
+            typeMenu="haut"
+            couleur="(--color-principal-300)"
+          />
+        </button>
+      </div>
     </nav>
   );
 }
