55e79a391b953e4979e81bdd95441f2bff1fd79e
Danilo Costa <dancc86@gmail.com>
2025-11-19 16:22:51 +0000

    Refactorisation du footer en display grid pour assurer un espacement cohérent entre les éléments.
    Suppression du lien de la barre de recherche dans le footer et déplacement de celle-ci dans le menu en haut.

--- frontend/src/components/components-partages/MenuEnBas/MenuEnBas.jsx
+++ frontend/src/components/components-partages/MenuEnBas/MenuEnBas.jsx
@@ -2,37 +2,18 @@ import Icon from "../Icon/Icon";
 import { Link } from "react-router-dom";
 function MenuEnBas({}) {
   return (
-    <footer className="flex justify-around fixed bottom-0 max-w-[500px] mx-auto inset-x-0 p-(--rythme-base) bg-(--color-principal-300)">
+    <footer className="grid grid-cols-4 fixed bottom-0 max-w-[500px] mx-auto inset-x-0 p-[var(--rythme-base)] bg-[var(--color-principal-300)]">
       <Link href="#">
-        <Icon
-          nom="accueil"
-          couleur="(--color-principal-100)"
-          typeMenu="bas"
-        />
+        <Icon nom="accueil" couleur="(--color-principal-100)" typeMenu="bas" />
       </Link>
       <Link href="#">
-        <Icon
-          nom="cellier"
-          couleur="(--color-principal-100)"
-          typeMenu="bas"
-        />
+        <Icon nom="cellier" couleur="(--color-principal-100)" typeMenu="bas" />
       </Link>
       <Link href="#">
         <Icon nom="liste" couleur="(--color-principal-100)" typeMenu="bas" />
       </Link>
       <Link href="#">
-        <Icon
-          nom="recherche"
-          couleur="(--color-principal-100)"
-          typeMenu="bas"
-        />
-      </Link>
-      <Link href="#">
-        <Icon
-          nom="profil"
-          couleur="(--color-principal-100)"
-          typeMenu="bas"
-        />
+        <Icon nom="profil" couleur="(--color-principal-100)" typeMenu="bas" />
       </Link>
     </footer>
   );
