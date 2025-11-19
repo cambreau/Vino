d09efaa43407f5a0320cf26c9dffdb1b48e14cc9
Chad Vezina <chadvezina@hotmail.com>
2025-11-19 09:33:24 -0500

    Creation modele,controller,routes bouteille + pool et MaJ de package

---
+++ backend/src/controllers/controller.bouteille.js
@@ -0,0 +1,32 @@
+/**
+ * @source backend/src/controllers/controller.bouteille.js
+ * Objectif:
+ * Orchestrer les flux REST autour du domaine bouteille et exposer
+ * explicitement dependances et reponses attendues.
+ */
+
+import modeleBouteille from "../models/modele.bouteille.js";
+
+export const listerBouteilles = async (req, res) => {
+
+};
+
+export const recupererBouteille = async (req, res) => {
+
+};
+
+export const creerBouteille = async (req, res) => {
+
+};
+
+export const modifierBouteille = async (req, res) => {
+
+};
+
+export const supprimerBouteille = async (req, res) => {
+
+};
+
+export const importerBouteillesDepuisSAQ = async (req, res) => {
+
+};
---
+++ backend/src/models/modele.bouteille.js
@@ -0,0 +1,66 @@
+/**
+ * @source backend/src/models/modele.bouteille.js
+ * Objectif:
+ * Implémenter la couche d'accès et de normalisation des bouteilles en
+ * documentant invariants, dépendances et opérations exposées.
+ */
+
+import pool from "../database/pool.js";
+
+const BASE_SELECT = `
+	SELECT
+		b.id_bouteille,
+		b.code_saq,
+		b.nom,
+		b.millenisme,
+		b.region,
+		b.cepage,
+		b.image,
+		b.description,
+		b.taux_alcool,
+		b.prix,
+		p.nom AS pays,
+		t.couleur AS type
+	FROM bouteille b
+	LEFT JOIN pays p ON p.id_pays = b.id_pays
+	LEFT JOIN type t ON t.id_type = b.id_type
+`;
+
+const mapper = (row) => ({
+  id: row.id_bouteille,
+  codeSAQ: row.code_saq,
+  nom: row.nom,
+  millesime: row.millenisme,
+  region: row.region,
+  cepage: row.cepage,
+  image: row.image,
+  description: row.description,
+  tauxAlcool: row.taux_alcool,
+  prix: row.prix,
+  pays: row.pays,
+  type: row.type,
+});
+
+class ModeleBouteille {
+  static async trouverTout() {}
+
+  static async trouverParId() {}
+
+  static async creer() {}
+
+  static async mettreAJour() {}
+
+  static async supprimer() {}
+
+  static async importerDepuisEnregistrementsSaq() {}
+
+  static async #normaliserPayload() {}
+
+  static async #mapperEnregistrementSaq() {}
+
+  static #construireDescription() {}
+
+  static async #assurerType() {}
+}
+
+export default ModeleBouteille;
