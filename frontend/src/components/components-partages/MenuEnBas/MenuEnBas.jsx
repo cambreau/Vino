bf6d7ad81dddaff503a8d04671d69b7381d50baa
Danilo Costa <dancc86@gmail.com>
2025-11-19 12:47:22 +0000

    Changer les balises anchor par Link

--- frontend/src/components/components-partages/MenuEnBas/MenuEnBas.jsx
+++ frontend/src/components/components-partages/MenuEnBas/MenuEnBas.jsx
@@ -1,23 +1,23 @@
 import Icon from "../Icon/Icon";
-
+import { Link } from "react-router-dom";
 function MenuEnBas({}) {
   return (
     <footer className="flex justify-around fixed bottom-0 w-full p-[var(--rythme-base)] bg-[var(--color-principal-300)]">
-      <a href="#">
+      <Link href="#">
         <Icon nom="accueil" couleur="var(--color-principal-100)" />
-      </a>
-      <a href="#">
+      </Link>
+      <Link href="#">
         <Icon nom="cellier" couleur="var(--color-principal-100)" />
-      </a>
-      <a href="#">
+      </Link>
+      <Link href="#">
         <Icon nom="liste" couleur="var(--color-principal-100)" />
-      </a>
-      <a href="#">
+      </Link>
+      <Link href="#">
         <Icon nom="recherche" couleur="var(--color-principal-100)" />
-      </a>
-      <a href="#">
+      </Link>
+      <Link href="#">
         <Icon nom="profil" couleur="var(--color-principal-100)" />
-      </a>
+      </Link>
     </footer>
   );
 }
