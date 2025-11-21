e606a6b9393f023856fde9bafd26412104fb2521
Danilo Costa <dancc86@gmail.com>
2025-11-21 09:31:52 +0000

    Creation de la fonction pour trouver un utilisateur par id

--- backend/src/models/modele.utilisateur.js
+++ backend/src/models/modele.utilisateur.js
@@ -25,26 +25,57 @@ export default class modeleUtilisateur {
     return lignes[0] || null;
   }
 
-
-/**
- * Fonction qui recherche un utilisateur par son courriel pour la connexion
-*/
-static async connexionUtilisateur(courriel) {
-  try {
+  //Récupérer un utilisateur par courriel
+  static async trouverParId(id) {
     const sql = `
+        select * from utilisateur where id = ? LIMIT 1
+        `;
+    const [lignes] = await connexion.execute(sql, [id]);
+    return lignes[0] || null;
+  }
+
+  /**
+   * Fonction qui recherche un utilisateur par son courriel pour la connexion
+  */
+  static async connexionUtilisateur(courriel) {
+    try {
+      const sql = `
       SELECT id_utilisateur, nom, courriel, mot_de_passe 
       FROM utilisateur 
       WHERE courriel = ? 
       LIMIT 1
     `;
-    const [resultat] = await connexion.execute(sql, [courriel]);
-    
-    // Si un utilisateur est trouvé, retourner le premier résultat, sinon retourner null
-    return resultat.length > 0 ? resultat[0] : null;
-  } catch (erreur) {
-    console.error('Erreur lors de la recherche de l\'utilisateur:', erreur);
-    throw erreur;
+      const [resultat] = await connexion.execute(sql, [courriel]);
+
+      // Si un utilisateur est trouvé, retourner le premier résultat, sinon retourner null
+      return resultat.length > 0 ? resultat[0] : null;
+    } catch (erreur) {
+      console.error('Erreur lors de la recherche de l\'utilisateur:', erreur);
+      throw erreur;
+    }
   }
-}
+
+  //Modifier un utilisateur et retourne si succès
+  static async modifier(id, nom, courriel, mot_de_passe_hache = null) {
+    // Si on ne change pas le mot de passe
+    if (!mot_de_passe_hache) {
+      const sql = `
+      UPDATE utilisateur 
+      SET nom = ?, courriel = ? 
+      WHERE id = ?
+    `;
+      const [resultat] = await connexion.execute(sql, [nom, courriel, id]);
+      return resultat.affectedRows > 0;
+    }
+
+    // Si on change aussi le mot de passe
+    const sql = `
+    UPDATE utilisateur 
+    SET nom = ?, courriel = ?, mot_de_passe = ? 
+    WHERE id = ?
+  `;
+    const [resultat] = await connexion.execute(sql, [nom, courriel, mot_de_passe_hache, id]);
+    return resultat.affectedRows > 0;
+  }
 }
 
