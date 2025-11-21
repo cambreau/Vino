667344f38dbf92df45be8e9fb7de384dac5e9161
cambreau <breau.camille76@gmail.com>
2025-11-21 08:55:29 -0500

    Correction href menu du bas

--- frontend/src/components/components-partages/MenuEnBas/MenuEnBas.jsx
+++ frontend/src/components/components-partages/MenuEnBas/MenuEnBas.jsx
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 function MenuEnBas({}) {
   return (
     <footer className="grid grid-cols-4 fixed bottom-0 max-w-[500px] mx-auto inset-x-0 p-[var(--rythme-base)] bg-[var(--color-principal-300)]">
-      <Link href="#">
+      <Link href="/">
         <Icon nom="accueil" couleur="(--color-principal-100)" typeMenu="bas" />
       </Link>
+      {/* ATTENTION LIEN A FAIRE */}
       <Link href="#">
         <Icon nom="cellier" couleur="(--color-principal-100)" typeMenu="bas" />
       </Link>
+      {/* ATTENTION LIEN A FAIRE */}
       <Link href="#">
         <Icon nom="liste" couleur="(--color-principal-100)" typeMenu="bas" />
       </Link>
-      <Link href="#">
+      <Link href="/profil">
         <Icon nom="profil" couleur="(--color-principal-100)" typeMenu="bas" />
       </Link>
     </footer>
