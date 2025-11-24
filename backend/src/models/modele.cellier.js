f31ef07bf90ca99e286c97de944b0c37a972d8d1
GabMaisonneuve <e2496359@cmaisonneuve.qc.ca>
2025-11-24 14:25:49 -0500

    Ajout de commentaires dans la fonction ajouter dans modeleCellier

--- backend/src/models/modele.cellier.js
+++ backend/src/models/modele.cellier.js
@@ -3,22 +3,27 @@ import { connexion } from "../database/connexion.js";
 export default class ModeleCellier {
   // Requête pour ajouter un cellier avec nom par défaut si non fourni
   static async ajouter(nom = "Mon Cellier", id_utilisateur) {
+    // S'assure que le id est un nombre
     const id = Number.parseInt(id_utilisateur, 10);
     if (!Number.isInteger(id) || id <= 0) {
       throw new Error("ID utilisateur invalide");
     }
 
+    // Lance une erreur si on ne trouve pas d'id ou non fourni
     if (!id_utilisateur) {
       throw new Error("Utilisateur requis pour créer un cellier");
     }
 
+    // S'assure que le nom du cellier n'est pas vide
     if (typeof nom !== "string" || nom.trim().length === 0) {
       nom = "Mon Cellier";
     }
 
+    // Requête pour créer un cellier
     const sql = `INSERT INTO cellier (id_utilisateur, nom) VALUES (?, ?)`;
     const [resultat] = await connexion.query(sql, [id, nom.trim()]);
 
+    // Retourne le nouveau Id créer avec au moment de la requête
     return resultat.insertId;
   }
   // Requête pour récupérer les celliers d'un utilisateur
