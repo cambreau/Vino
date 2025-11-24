89946f1f2010305d1415bdc2ede8f2d0e35c37a6
GabMaisonneuve <e2496359@cmaisonneuve.qc.ca>
2025-11-24 14:50:38 -0500

    Ajout de commentaires pour fonction AjouterCellier

--- backend/src/controllers/controller.cellier.js
+++ backend/src/controllers/controller.cellier.js
@@ -2,15 +2,19 @@ import ModeleCellier from "../models/modele.cellier.js";
 
 export const ajouterCellier = async (req, res) => {
   try {
+    // Nom par défaut si non fourni
     const { id_utilisateur, nom = "Mon Cellier" } = req.body;
 
+    // Retour d'erreur si Id non trouvé
     if (!id_utilisateur) {
       return res.status(400).json({
         message: "Id utilisateur requis pour créer un cellier",
       });
     }
+    // Action d'ajout de cellier, l'id est récupéré grâce à return insertId dans le model
     const id = await ModeleCellier.ajouter(nom, id_utilisateur);
 
+    // Message de succès si tout est OK
     res.status(201).json({
       message: "Cellier créé avec succès",
       id,
