3cf881e9435bbcd3c8c75838ddf1d8587e9edb9e
GabMaisonneuve <e2496359@cmaisonneuve.qc.ca>
2025-11-24 14:49:17 -0500

    Finalisation fonction ajouterCellier dans le controller

--- backend/src/controllers/controller.cellier.js
+++ backend/src/controllers/controller.cellier.js
@@ -1,6 +1,27 @@
 import ModeleCellier from "../models/modele.cellier.js";
 
-export const ajouterCellier = async (req, res) => {};
+export const ajouterCellier = async (req, res) => {
+  try {
+    const { id_utilisateur, nom = "Mon Cellier" } = req.body;
+
+    if (!id_utilisateur) {
+      return res.status(400).json({
+        message: "Id utilisateur requis pour créer un cellier",
+      });
+    }
+    const id = await ModeleCellier.ajouter(nom, id_utilisateur);
+
+    res.status(201).json({
+      message: "Cellier créé avec succès",
+      id,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Erreur serveur",
+    });
+  }
+};
 
 export const recupererCellier = async (req, res) => {};
 
