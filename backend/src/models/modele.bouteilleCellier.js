3a86e6cbe1a854781fa7b23eb80c4ebc6251bd6a
GabMaisonneuve <e2496359@cmaisonneuve.qc.ca>
2025-11-24 13:34:40 -0500

    Fonction ajouter une bouteille au cellier finaliser

--- backend/src/models/modele.bouteilleCellier.js
+++ backend/src/models/modele.bouteilleCellier.js
@@ -5,7 +5,24 @@ export default class modeleBouteilleCellier {
   static async recuperer(idCellier) {}
 
   // Requête pour ajouter une bouteille dans un cellier
-  static async ajouter(idCellier, idBouteille, quantite, notes) {}
+  static async ajouter(idCellier, idBouteille, quantite = 1) {
+    if (!idCellier || !idBouteille)
+      throw new Error("Un cellier est une bouteille est nécessaire");
+
+    if (typeof quantite !== "number" || quantite <= 0) quantite = 1;
+
+    const sql = `
+    INSERT INTO bouteilleCellier (id_cellier, id_bouteille, quantite)
+    VALUES (?, ?, ?)
+  `;
+
+    const [result] = await connexion.query(sql, [
+      idCellier,
+      idBouteille,
+      quantite,
+    ]);
+    return result.insertId;
+  }
 
   // Requête pour Modifier quantité, notes, etc.
   static async modifier(idCellier, idBouteilles, quantite, notes) {}
