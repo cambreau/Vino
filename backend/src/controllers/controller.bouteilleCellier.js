291f682b66235c0b0a80e75c93a0f3f96815c905
GabMaisonneuve <e2496359@cmaisonneuve.qc.ca>
2025-11-24 13:36:35 -0500

    Fonction ajouterBouteilleCellier finaliser

--- backend/src/controllers/controller.bouteilleCellier.js
+++ backend/src/controllers/controller.bouteilleCellier.js
@@ -10,6 +10,37 @@ export const afficherBouteilleDuCellier = async (req, res) => {};
 
 export const modifierBouteilleDuCellier = async (req, res) => {};
 
-export const ajouterBouteilleDuCellier = async (req, res) => {};
+export const ajouterBouteilleDuCellier = async (req, res) => {
+  try {
+    const { idCellier } = req.params;
+    const identifiantCellier = Number.parseInt(idCellier, 10);
+
+    const { idBouteille } = req.body;
+    const identifiantBouteille = Number.parseInt(idBouteille, 10);
+
+    let { quantite = 1 } = req.body;
+    quantite = Number.parseInt(quantite, 10);
+
+    // Validation des ID
+    if (!identifiantCellier || !identifiantBouteille) {
+      return res
+        .status(400)
+        .json({ message: "ID cellier et ID bouteille requis" });
+    }
+
+    const action = await modeleBouteilleCellier.ajouter(
+      identifiantCellier,
+      identifiantBouteille,
+      quantite
+    );
+
+    res
+      .status(201)
+      .json({ message: "Bouteille ajoutée au cellier", id: action });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Erreur serveur" });
+  }
+};
 
 export const supprimerBouteilleDuCellier = async (req, res) => {};
