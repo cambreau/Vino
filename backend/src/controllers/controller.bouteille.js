fb1d03f6bba96fa48ce8a95f7ae44ab13d477c28
GabMaisonneuve <e2496359@cmaisonneuve.qc.ca>
2025-11-24 10:16:19 -0500

    Fonction modifierBouteille dans le controller bouteille finaliser

--- backend/src/controllers/controller.bouteille.js
+++ backend/src/controllers/controller.bouteille.js
@@ -79,7 +79,38 @@ export const creerBouteille = async (req, res) => {
   }
 };
 
-export const modifierBouteille = async (req, res) => {};
+export const modifierBouteille = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const identifiant = Number.parseInt(id, 10);
+
+    if (!Number.isInteger(identifiant) || identifiant <= 0) {
+      return res.status(400).json({
+        message: "Identifiant de bouteille invalide",
+      });
+    }
+    const action = await modeleBouteille.mettreAJour(identifiant, req.body);
+
+    if (!action) {
+      return res.status(404).json({
+        message: "Bouteille introuvable ou aucune modification effectuée",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      action,
+      message: "Bouteille modifié avec succès",
+    });
+  } catch (error) {
+    console.error("Erreur lors de la modification d'une bouteille", error);
+
+    return res.status(400).json({
+      success: false,
+      message: "Impossible de modifier la bouteille",
+    });
+  }
+};
 
 export const supprimerBouteille = async (req, res) => {};
 
