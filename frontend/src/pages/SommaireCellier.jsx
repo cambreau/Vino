85370f00d31cff58c6c30a0a3595d3469a5f958b
cambreau <breau.camille76@gmail.com>
2025-11-24 21:01:14 -0500

    Modification du css de l image de fond

--- frontend/src/pages/SommaireCellier.jsx
+++ frontend/src/pages/SommaireCellier.jsx
@@ -8,10 +8,12 @@ function SommaireCellier() {
         <MenuEnHaut titre="Sommaire cellier" />
       </header>
       <main
-        className="min-h-screen font-body max-w-[500px] mx-auto inset-x-0 
-      bg-[linear-gradient(0deg,rgba(0,0,0,0.7)25%,rgba(0,0,0,0)),url('../assets/images/sommaireCellier.webp')] bg-cover bg-centerbg-fond"
+        className="min-h-screen font-body max-w-[500px] mx-auto inset-x-0 relative
+      bg-[linear-gradient(0deg,rgba(0,0,0,0.05)25%,rgba(0,0,0,0)),url('../assets/images/sommaireCellier.webp')] bg-cover bg-center bg-fond"
       >
-        <section className="pt-(--rythme-espace) pb-(--rythme-base) px-(--rythme-serre)"></section>
+        <div className="absolute inset-0 bg-white/40 pointer-events-none"></div>
+
+        <section className="relative pt-(--rythme-espace) pb-(--rythme-base) px-(--rythme-serre)"></section>
       </main>
       <footer>
         <MenuEnBas />
