80f429b3c4a6b1cde998a04a889a1d832d98af7a
cambreau <breau.camille76@gmail.com>
2025-11-24 20:56:53 -0500

    Correction routes Catalogue

--- frontend/src/components/components-partages/MenuEnBas/MenuEnBas.jsx
+++ frontend/src/components/components-partages/MenuEnBas/MenuEnBas.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function MenuEnBas({}) {
   return (
     <footer className="grid grid-cols-4 mt-auto max-w-[500px] mx-auto inset-x-0 p-[var(--rythme-base)] bg-[var(--color-principal-300)]">
-      <Link href="/Catalogue">
+      <Link to="/catalogue">
         <Icon
           nom="catalogue"
           couleur="(--color-principal-100)"
