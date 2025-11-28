e251cfdfeab8f5f5ccfce19ccbf5d08ec550e324
ChadVezina <125046379+ChadVezina@users.noreply.github.com>
2025-11-28 15:59:08 -0500

    Simplification en ajoutant la page connexion à la page bienvenue

--- frontend/src/components/components-partages/MenuEnHaut/MenuEnHaut.jsx
+++ frontend/src/components/components-partages/MenuEnHaut/MenuEnHaut.jsx
@@ -6,135 +6,153 @@ import RasinLogo from "@assets/images/grape_logo.svg";
 import authentificationStore from "@store/authentificationStore";
 
 function MenuEnHaut({}) {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  // Récupérer les données du store
-  const utilisateur = authentificationStore((state) => state.utilisateur);
-  const estConnecte = authentificationStore((state) => state.estConnecte);
+	// Récupérer les données du store
+	const utilisateur = authentificationStore((state) => state.utilisateur);
+	const estConnecte = authentificationStore((state) => state.estConnecte);
 
-  const [estMenuOuvert, setestMenuOuvert] = useState(false);
+	const [estMenuOuvert, setestMenuOuvert] = useState(false);
 
-  /**
-   * Fonction pour gérer la déconnexion
-   */
-  const gererDeconnexion = () => {
-    authentificationStore.getState().deconnexion();
-    navigate("/connexion?deconnexionSucces=true");
-  };
+	/**
+	 * Fonction pour gérer la déconnexion
+	 */
+	const gererDeconnexion = () => {
+		authentificationStore.getState().deconnexion();
+		navigate("/?deconnexionSucces=true");
+	};
 
-  return (
-    <nav className="flex items-center justify-between p-(--rythme-base) bg-fond-secondaire">
-      <div className="relative">
-        {/* Bouton hamburger */}
-        <button
-          onClick={() => setestMenuOuvert(!estMenuOuvert)}
-          aria-label={estMenuOuvert ? "Fermer le menu" : "Ouvrir le menu"}
-          aria-expanded={estMenuOuvert}
-        >
-          <Icon
-            nom="menuHamburger"
-            typeMenu="haut"
-            couleur="(--color-principal-300)"
-          />
-        </button>
+	return (
+		<nav className="flex items-center justify-between p-(--rythme-base) bg-fond-secondaire">
+			<div className="relative">
+				{/* Bouton hamburger */}
+				<button
+					onClick={() => setestMenuOuvert(!estMenuOuvert)}
+					aria-label={
+						estMenuOuvert ? "Fermer le menu" : "Ouvrir le menu"
+					}
+					aria-expanded={estMenuOuvert}>
+					<Icon
+						nom="menuHamburger"
+						typeMenu="haut"
+						couleur="(--color-principal-300)"
+					/>
+				</button>
 
-        {/* Ombre */}
-        {estMenuOuvert && (
-          <div
-            className="fixed inset-0 bg-black/50"
-            onClick={() => setestMenuOuvert(false)}
-          />
-        )}
+				{/* Ombre */}
+				{estMenuOuvert && (
+					<div
+						className="fixed inset-0 bg-black/50"
+						onClick={() => setestMenuOuvert(false)}
+					/>
+				)}
 
-        {/* Menu déroulant */}
-        {estMenuOuvert && (
-          <div className="absolute -left-(--rythme-base) mt-(--rythme-tres-serre) p-(--rythme-base) h-screen  min-w-[300px] bg-fond-secondaire">
-            <div className="flex justify-between mb-(--rythme-espace)">
-              <header>
-                <h2 className="text-texte-premier text-(length:--taille-grand) font-display font-bold">
-                  {estConnecte && utilisateur ? utilisateur.nom : ""}
-                </h2>
-                <small className="text-texte-premier text-(length:--taille-moyen) font-display">
-                  {estConnecte && utilisateur
-                    ? utilisateur.courriel
-                    : "Courriel"}
-                </small>
-              </header>
-              <button
-                className="absolute right-(--rythme-base) top-(--rythme-serre)"
-                onClick={() => setestMenuOuvert(!estMenuOuvert)}
-                aria-label={estMenuOuvert ? "Fermer le menu" : "Ouvrir le menu"}
-                aria-expanded={estMenuOuvert}
-              >
-                <Icon
-                  nom="fermer"
-                  typeMenu="haut"
-                  couleur="(--color-principal-300)"
-                />
-              </button>
-            </div>
-            <div className="flex flex-col gap-(--rythme-base)">
-              <Link to="/profil" onClick={() => setestMenuOuvert(false)}>
-                <Icon
-                  nom="profil"
-                  typeMenu="haut"
-                  couleur="(--color-principal-300)"
-                />
-              </Link>
-              <Link to="/catalogue" onClick={() => setestMenuOuvert(false)}>
-                <Icon
-                  nom="catalogue"
-                  typeMenu="haut"
-                  couleur="(--color-principal-300)"
-                />
-              </Link>
-              <Link
-                to="/sommaire-cellier"
-                onClick={() => setestMenuOuvert(false)}
-              >
-                <Icon
-                  nom="cellier"
-                  typeMenu="haut"
-                  couleur="(--color-principal-300)"
-                />
-              </Link>
-              <Link to="#" onClick={() => setestMenuOuvert(false)}>
-                <Icon
-                  nom="liste"
-                  typeMenu="haut"
-                  couleur="(--color-principal-300)"
-                />
-              </Link>
-            </div>
-          </div>
-        )}
-      </div>
+				{/* Menu déroulant */}
+				{estMenuOuvert && (
+					<div className="absolute -left-(--rythme-base) mt-(--rythme-tres-serre) p-(--rythme-base) h-screen  min-w-[300px] bg-fond-secondaire">
+						<div className="flex justify-between mb-(--rythme-espace)">
+							<header>
+								<h2 className="text-texte-premier text-(length:--taille-grand) font-display font-bold">
+									{estConnecte && utilisateur
+										? utilisateur.nom
+										: ""}
+								</h2>
+								<small className="text-texte-premier text-(length:--taille-moyen) font-display">
+									{estConnecte && utilisateur
+										? utilisateur.courriel
+										: "Courriel"}
+								</small>
+							</header>
+							<button
+								className="absolute right-(--rythme-base) top-(--rythme-serre)"
+								onClick={() => setestMenuOuvert(!estMenuOuvert)}
+								aria-label={
+									estMenuOuvert
+										? "Fermer le menu"
+										: "Ouvrir le menu"
+								}
+								aria-expanded={estMenuOuvert}>
+								<Icon
+									nom="fermer"
+									typeMenu="haut"
+									couleur="(--color-principal-300)"
+								/>
+							</button>
+						</div>
+						<div className="flex flex-col gap-(--rythme-base)">
+							<Link
+								to="/profil"
+								onClick={() => setestMenuOuvert(false)}>
+								<Icon
+									nom="profil"
+									typeMenu="haut"
+									couleur="(--color-principal-300)"
+								/>
+							</Link>
+							<Link
+								to="/catalogue"
+								onClick={() => setestMenuOuvert(false)}>
+								<Icon
+									nom="catalogue"
+									typeMenu="haut"
+									couleur="(--color-principal-300)"
+								/>
+							</Link>
+							<Link
+								to="/sommaire-cellier"
+								onClick={() => setestMenuOuvert(false)}>
+								<Icon
+									nom="cellier"
+									typeMenu="haut"
+									couleur="(--color-principal-300)"
+								/>
+							</Link>
+							<Link
+								to="#"
+								onClick={() => setestMenuOuvert(false)}>
+								<Icon
+									nom="liste"
+									typeMenu="haut"
+									couleur="(--color-principal-300)"
+								/>
+							</Link>
+						</div>
+					</div>
+				)}
+			</div>
 
-      {/* Logo */}
-      <Link to="/catalogue" className="flex items-center">
-        <h2 className="text-principal-300 text-(length:--taille-grand) font-display">
-          Vin
-        </h2>
-        <img src={RasinLogo} alt="Logo raisin" width="43" height="35" />
-      </Link>
+			{/* Logo */}
+			<Link
+				to="/catalogue"
+				className="flex items-center">
+				<h2 className="text-principal-300 text-(length:--taille-grand) font-display">
+					Vin
+				</h2>
+				<img
+					src={RasinLogo}
+					alt="Logo raisin"
+					width="43"
+					height="35"
+				/>
+			</Link>
 
-      {/* Déconnexion */}
-      {estConnecte && (
-        <Bouton
-          texte={
-            <Icon
-              nom="deconnection"
-              typeMenu="haut"
-              couleur="(--color-principal-300)"
-            />
-          }
-          type="secondaire"
-          typeHtml="button"
-          action={gererDeconnexion}
-        />
-      )}
-    </nav>
-  );
+			{/* Déconnexion */}
+			{estConnecte && (
+				<Bouton
+					texte={
+						<Icon
+							nom="deconnection"
+							typeMenu="haut"
+							couleur="(--color-principal-300)"
+						/>
+					}
+					type="secondaire"
+					typeHtml="button"
+					action={gererDeconnexion}
+				/>
+			)}
+		</nav>
+	);
 }
 
 export default MenuEnHaut;
