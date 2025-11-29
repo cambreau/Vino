c81c4a8fa9f2b6bf19b24dc4a0d2c69ce0c74a1d
ChadVezina <125046379+ChadVezina@users.noreply.github.com>
2025-11-29 17:23:05 -0500

    Creation premier cellier frontend

--- frontend/src/lib/validationFormulaire.js
+++ frontend/src/lib/validationFormulaire.js
@@ -3,6 +3,7 @@ export const regex = {
   regcourriel: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
   //- au moins 8 caractères, au moins une lettre minuscule, au moins une lettre majuscule, au moins un caractère spécial (ex. !@#$%^&*())
   regMotDePasse: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9]).{8,}$/,
+  regNomCellier: /^[A-Za-zÀ-ÖØ-öø-ÿ0-9\s'-]{2,50}$/,
 };
 
 export const validationChamp = (regEx, valeur) => {
